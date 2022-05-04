af6b462eaeb05f195cf44f9c04259e39ed55908b
Meapy <dkrasovskijj@gmail.com>
2022-05-04 16:40:45 +0100

    spaceships flying in formation

---
+++ Assets/Scripts/Convoy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Convoy : MonoBehaviour
+{
+    public GameObject prefab;
+
+    [Range (1, 5000)]
+    public int radius = 50;
+    
+    public int count = 10;
+    public GameObject health;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(spawnHealth());
+        //add the Spaceship component to all the child objects
+        foreach (Transform child in transform)
+        {
+            child.gameObject.AddComponent<Spaceship>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator spawnHealth()
+    {
+        yield return new WaitForSeconds(5f);
+        //instantiate health
+        GameObject health = GameObject.Instantiate(this.health);
+        //set the position to spawn at random within the spawn radius
+        Vector2 position = Random.insideUnitCircle * 20;
+        //set the hight to be random 
+        float height = Random.Range(0, 10);
+        //set the position of the health
+        health.transform.position = new Vector3(position.x, height, position.y);
+        
+        StartCoroutine(spawnHealth());
+    }
+}
---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    public int length = 5;
+    public Material material;
+
+
+
+    void Awake()
+    {
+        
+        gameObject.AddComponent<Boid>();
+        gameObject.AddComponent<NoiseWander>().axis = NoiseWander.Axis.Horizontal;
+        gameObject.AddComponent<NoiseWander>().axis = NoiseWander.Axis.Vertical;
+        gameObject.AddComponent<ObstacleAvoidance>();
+        gameObject.AddComponent<Constrain>();
+        //add the boid component to this object
+        
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
